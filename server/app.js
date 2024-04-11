f58d39cafd424955f1456a225973065cfa29618b
Nwaigba66 <your.email@glorianwaigba@gmail.com>
2024-04-11 14:30:29 +0200

    added app.js

--- server/app.js
+++ server/app.js
@@ -28,12 +28,13 @@ app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+const connection = process.env.connection || "mongodb://127.0.0.1:27017/triphaven"
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/triphaven")
+  .connect(connection)
   .then(x => console.log(`Connected to Database: "${x.connections[0].name}"`))
   .catch(err => console.error("Error connecting to MongoDB", err));
+
 
 
 
